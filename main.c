9bef0f6ca0f04273ec231a8fb994b0c1bf5778c2
serge Mbele <36066205+serges007@users.noreply.github.com>
2018-04-06 17:21:25 +0200

    Add files via upload
    ouvrir le terminal sous linux et tapez la commande: 
    1 - gcc -o main main.c
    2 - ./main

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include"struct.h"
+#include"prototypeAdh.h"
+#include"prototypeLiv.h"
+#include"prototypeEm.h"
+#include"adherent.h"
+#include"livre.h"
+#include"emprunt.h"
+#include"close.h"
+
+int main(){
+	menu();
+	return 0;
+}
